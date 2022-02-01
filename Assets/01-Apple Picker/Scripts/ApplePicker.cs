d492412fcbfa90da7ef3738d516fb931ca5878bc
RyanS1475 <rjs425@scarletmail.rutgers.edu>
2022-02-01 16:56:38 -0500

    Destroy Baskets
    Baskets are now destroyed when an apple passes by. Score counter also works

--- Assets/01-Apple Picker/Scripts/ApplePicker.cs
+++ Assets/01-Apple Picker/Scripts/ApplePicker.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
+    [Header("Set in Inspector")]
     public GameObject basketPrefab;
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
     // Start is called before the first frame update
     void Start()
     {
+        basketList = new List<GameObject>();
+
         // creates baskets
         for (int i = 0; i < numBaskets; i++)
         {
@@ -19,7 +24,8 @@ public class ApplePicker : MonoBehaviour
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
-        }
+            basketList.Add(tBasketGO);
+       }
     }
     public void AppleDestroyed()
     {
@@ -28,6 +34,11 @@ public class ApplePicker : MonoBehaviour
         {
             Destroy(tGO);
         }
+
+        int basketIndex = basketList.Count - 1;
+        GameObject tBasketGO = basketList[basketIndex];
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
     }
 
     // Update is called once per frame
