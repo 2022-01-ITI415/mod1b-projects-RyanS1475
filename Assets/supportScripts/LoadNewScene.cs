af9c8c51688180b56ca27983ac0321dafd4e5da0
RyanS1475 <rjs425@scarletmail.rutgers.edu>
2022-02-03 20:34:09 -0500

    Built Wrong Version and Fixed it
    Fixed it so my scene is the one that is loaded

--- Assets/supportScripts/LoadNewScene.cs
+++ Assets/supportScripts/LoadNewScene.cs
@@ -8,7 +8,7 @@ public class LoadNewScene : MonoBehaviour
 
     public void LoadScene1()
     {
-        SceneManager.LoadScene("Main-ApplePicker");
+        SceneManager.LoadScene("Ryan Sgro Apple Picker");
     }
 
     public void LoadScene2()
