44209e37eb4d5545e974430a8f204d458ce82e67
RyanS1475 <rjs425@scarletmail.rutgers.edu>
2022-02-17 17:52:58 -0500

    BestTime Script
    This was an attempt to make a script that keeps the best time for the player as a high score

---
+++ Assets/03-Prototype1/Scripts/BestTime.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BestTime : MonoBehaviour
+{
+    static public int crntTime = 00:00.000;
+
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey("BestTime"))
+        {
+            crntTime = PlayerPrefs.GetInt("BestTime");
+        }
+        PlayerPrefs.SetInt("BestTime", crntTime);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text gt = this.GetComponent<Text>();
+        gt.text = "Best Time: " + crntTime;
+        if (crntTime > PlayerPrefs.GetInt("BestTime"))
+        {
+            PlayerPrefs.SetInt("BestTime", crntTime);
+        }
+    }
+}
