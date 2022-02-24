4fbd3e75019aaa1809ca188db8d803e2ba8491f6
RyanS1475 <rjs425@scarletmail.rutgers.edu>
2022-02-24 18:39:23 -0500

    More Script Edits
    Script stuff

--- Assets/03-Prototype1/Scripts/Projectile.cs
+++ Assets/03-Prototype1/Scripts/Projectile.cs
@@ -37,14 +37,8 @@ public class Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    private void FixedUpdate()
-    {
-        Vector3 fwd = transform.TransformDirection(Vector3.forward);
-
-        if (Physics.Raycast(transform.position, fwd, 10))
-            print("There is something in front of the object!");
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, 100))
+            print("Hit something!");
     }
 }
