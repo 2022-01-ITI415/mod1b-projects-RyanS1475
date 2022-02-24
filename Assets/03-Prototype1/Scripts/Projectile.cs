f7dbf00d237805de93f3e0a132e38b6733c8294f
RyanS1475 <rjs425@scarletmail.rutgers.edu>
2022-02-24 17:13:44 -0500

    New Script
    Going to try and make a new script for the projectile

--- Assets/03-Prototype1/Scripts/Projectile.cs
+++ Assets/03-Prototype1/Scripts/Projectile.cs
@@ -4,22 +4,6 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    public float speed = 10f;
-    Vector3 lastPosition;
-    public GameObject projectile;
-    public GameObject prefabProjectile;
-    private Rigidbody projectileRigidbody;
-    public Vector3 launchPos;
-
-    void OnMouseDown()
-    {
-        projectile = Instantiate(prefabProjectile) as GameObject;
-        projectile.transform.position = launchPos;
-        projectile.GetComponent<Rigidbody>().isKinematic = true;
-        projectileRigidbody = projectile.GetComponent<Rigidbody>();
-        projectileRigidbody.isKinematic = true;
-    }
-
     // Start is called before the first frame update
     void Start()
     {
@@ -29,22 +13,6 @@ public class Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos2D = Input.mousePosition;
-        mousePos2D.z = -Camera.main.transform.position.z;
-        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
-        Vector3 pos = this.transform.position;
-        pos.x = mousePos3D.x;
-        this.transform.position = pos;
-
-        //transform.position += transform.forward * speed * Time.deltaTime;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        GameObject collidedWith = collision.gameObject;
-        if (collidedWith.tag == "Target" || collidedWith.tag == "Target (1)" || collidedWith.tag == "Target (2)" || collidedWith.tag == "Target (3)" || collidedWith.tag == "Target (4)")
-        {
-            Destroy(collidedWith);
-        }
+        
     }
 }
