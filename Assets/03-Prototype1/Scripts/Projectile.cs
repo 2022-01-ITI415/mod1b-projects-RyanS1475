55d58b7983dc3fa6bb54f94837fa32b46482076d
RyanS1475 <rjs425@scarletmail.rutgers.edu>
2022-02-24 17:48:15 -0500

    Fixed Main Screen Title and Edited Script
    Changed the name of my game when the game is launched and also edited the projectile script

--- Assets/03-Prototype1/Scripts/Projectile.cs
+++ Assets/03-Prototype1/Scripts/Projectile.cs
@@ -1,9 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Projectile : MonoBehaviour
 {
+    public GameObject prefabProjectile;
+    public GameObject projectile;
+    public Vector3 launchPos;
+    private float movementX;
+    private float movementY;
+    public float velocityMult = 8f;
+    public bool aimingMode;
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +38,13 @@ public class Projectile : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, fwd, 10))
+            print("There is something in front of the object!");
     }
 }
